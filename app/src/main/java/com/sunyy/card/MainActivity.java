cf32ed4845f7dbfe16d195a989e71d36a2abb451
sun0816 <sunyy@knowbox.cn>
2018-05-03 11:38:04 +0800

    En:添加控制时间

--- app/src/main/java/com/sunyy/card/MainActivity.java
+++ app/src/main/java/com/sunyy/card/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
+    private EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,26 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 if (cardView.getAnimator() != null)
                     cardView.getAnimator().start();
+            }
+        });
+
+        mEdit = findViewById(R.id.edit);
+
+        findViewById(R.id.duration_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    long time = Long.valueOf(mEdit.getText().toString());
+                    if (cardView.getAnimator() != null) {
+                        cardView.getAnimator().setDuration(time);
+                        cardView.getAnimator().start();
+                    }
+                } catch (Exception e) {
+                    cardView.getAnimator().setDuration(1000);
+                    cardView.getAnimator().start();
+
+                }
+
             }
         });
     }
